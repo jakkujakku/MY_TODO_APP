aa53361b341024bd3da0cda59d9365e13a9685ad
jakkujakku <jakkujakku98@gmail.com>
2023-08-24 12:01:48 +0900

    메인 컨트롤러 업데이트

--- MY_TODO_APP/Controllers/MainController.swift
+++ MY_TODO_APP/Controllers/MainController.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 class MainController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
 
-    @IBOutlet weak var imageView: UIImageView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
+    @IBAction func tappedGoToToDoPageButton(_ sender: UIButton) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: Name.todoControllerIdentifier) as? ToDoController else { return }
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 
+    @IBAction func tappedGoToCompletionPageButton(_ sender: UIButton) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: Name.completionControllerIdentifier) as? CompletionController else { return }
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
-
---
+++ MY_TODO_APP/Models/Name.swift
@@ -0,0 +1,19 @@
+//
+//  Name.swift
+//  MY_TODO_APP
+//
+//  Created by (^ㅗ^)7 iMac on 2023/08/24.
+//
+
+import Foundation
+
+class Name {
+    // Controllers
+    static let mainControllerIdentifier = ""
+    static let todoControllerIdentifier = ""
+    static let completionControllerIdentifier = ""
+    
+    // Cells
+    static let todoCellIdentifier = ""
+    static let completionCellIdentifier = ""
+}
