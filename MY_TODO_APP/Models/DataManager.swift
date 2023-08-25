b0c0a357b99f3538a123a4e78c587afb383a675e
jakkujakku <jakkujakku98@gmail.com>
2023-08-25 14:25:42 +0900

    커밋

--- MY_TODO_APP/Models/DataManager.swift
+++ MY_TODO_APP/Models/DataManager.swift
@@ -8,16 +8,16 @@
 import Foundation
 
 class DataManager: Codable {
-//    static var dataManager = [dataManagerCopy0, dataManagerCopy1, dataManagerCopy2, dataManagerCopy3]
-    static var dataManager: [[ToDoData]] = [doDataManager, decideDataManager, delegateDataManager, deleteDataManager]
-    static var doDataManager: [ToDoData] = []
-    static var decideDataManager: [ToDoData] = []
-    static var delegateDataManager: [ToDoData] = []
-    static var deleteDataManager: [ToDoData] = []
-    static var completionDataManager: [ToDoData] = []
+    static var dataManager: [[ToDoData]] = []
+    static var doDataManager = [ToDoData]()
+    static var decideDataManager = [ToDoData]()
+    static var delegateDataManager = [ToDoData]()
+    static var deleteDataManager = [ToDoData]()
+    static var completionDataManager = [ToDoData]()
 
     static func saveToUserDefaults() {
         do {
+            dataManager = [doDataManager, decideDataManager, delegateDataManager, deleteDataManager]
             let encodedData = try JSONEncoder().encode(dataManager)
             UserDefaults.standard.set(encodedData, forKey: Utility.userDefaultsKey)
             print("\(encodedData) 저장 했습니다.")
@@ -30,6 +30,18 @@ class DataManager: Codable {
         if let encodedData = UserDefaults.standard.data(forKey: Utility.userDefaultsKey) {
             do {
                 dataManager = try JSONDecoder().decode([[ToDoData]].self, from: encodedData)
+                if dataManager.isEmpty == false {
+                    doDataManager = dataManager[0] // 신경써라... 아쎄이(동기화)
+                    decideDataManager = dataManager[1] // 신경써라... 아쎄이(동기화)
+                    delegateDataManager = dataManager[2] // 신경써라... 아쎄이(동기화)
+                    deleteDataManager = dataManager[3] // 신경써라... 아쎄이(동기화)
+                } else {
+                    doDataManager = []
+                    decideDataManager = []
+                    delegateDataManager = []
+                    deleteDataManager = []
+                }
+
                 print("\(dataManager) 를 불러왔습니다.")
             } catch {
                 print("Error decoding data: \(error)")
